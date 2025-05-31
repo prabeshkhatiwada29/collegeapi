63e7465d96ec50f349c570334fe2515583d7e1ec
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-05-31 20:37:28 +0545

    api

--- collegeapi/api/serailzers.py
+++ collegeapi/api/serailzers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 from api.models import College, student
 class CollegeSerializer(serializers.HyperlinkedModelSerializer):
+  
     class Meta:
         model = College
         fields = "__all__"  # Use double dashes to include all fields
