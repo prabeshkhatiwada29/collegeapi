a0833399704d89b9a3aab0430b4b18ceea5541ca
PIKU <160405297+prabeshkhatiwada29@users.noreply.github.com>
2025-06-01 08:32:27 +0545

    Colllegeapi

--- collegeapi/api/admin.py
+++ collegeapi/api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from api.models import College, student
+
 
 # Register your models here.
+
+class CollegeAdmin(admin.ModelAdmin):
+    list_display= ('name', 'location', 'established_year')
+    search_fields = ('name', 'location', )  
+
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'age', 'college', )
+    search_fields = ('name', 'college__name', )  # Search by student name and college name
+    list_filter = ('college', )  # Filter by college
+admin.site.register(College,CollegeAdmin)
+admin.site.register(student,StudentAdmin)
--- collegeapi/api/views.py
+++ collegeapi/api/views.py
@@ -2,12 +2,27 @@ from django.shortcuts import render
 from rest_framework import  viewsets
 from api.models import College,student
 from api.serailzers import CollegeSerializer, StudentSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
 
 
 # Create your views here.
 class CollegeViewset(viewsets.ModelViewSet):
   queryset=College.objects.all()
   serializer_class = CollegeSerializer
+
+  @action(detail=True, methods=['get'])
+  def students(self,request, pk=None):
+    try:
+      College=College.objects.get(pk=pk)
+      students = student.objects.filter(college=College)
+      student_serailizer = StudentSerializer(students, many=True)
+      return Response(student_serailizer.data)
+    except Exception as e:
+      return Response({"error": str(e)}, status=400)
+    
+
 
 
 class StudentViewset(viewsets.ModelViewSet):
